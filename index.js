73189ba7d8630707b3e57bba17dd48cf23adc185
Philipp Lerche <pler@de.ibm.com>
2017-01-13 19:13:09 +0100

    Improve voice responses

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ const UTTERANCES = {
 	[INTENT_RECEIVER_TURN_OFF]: [],
 	[INTENT_RECEIVER_TURN_ON]: [],
 	[INTENT_RECEIVER_SET_VOLUME_TO]: [
-		'setze lautstärke auf {-|VOLUME_LEVEL}'
+		'{setze|stelle} lautstärke {auf|zu} {-|VOLUME_LEVEL}'
 	]
 };
-
 
 const SLOTS = {
 	[INTENT_RECEIVER_TURN_OFF]: [],
@@ -81,45 +80,43 @@ app.sessionEnded(function(req, res) {
 // Intent: LDVPReceiverTurnOffIntent
 
 app.intent(INTENT_RECEIVER_TURN_OFF, _getIntentOpts(INTENT_RECEIVER_TURN_OFF), function (request, response) {
-	_sendRequest('GET', '/receiver/poweroff', {}, _expect([200], function (err, _, body) {
+	_sendRequest('GET', '/receiver/poweroff', {}, function (err, _, body) {
 		if (err) {
-			console.error(err);
-			response.say("fehler").send();
-			return;
+			response.say("Fehler").send();
 		}
-		console.log(body);
-		response.say("fertig").send();
-	}));
+		if (body.message) {
+			response.say(body.message).send();
+		}
+	});
 	return false;
 });
 
 // Intent: LDVPReceiverTurnOnIntent
 
 app.intent(INTENT_RECEIVER_TURN_ON, _getIntentOpts(INTENT_RECEIVER_TURN_ON), function (request, response) {
-	_sendRequest('GET', '/receiver/poweron', {}, _expect([200], function (err, _, body) {
+	_sendRequest('GET', '/receiver/poweron', {}, function (err, _, body) {
 		if (err) {
-			console.error(err);
-			response.say("fehler").send();
-			return;
+			response.say("Fehler").send();
 		}
-		console.log(body);
-		response.say("fertig").send();
-	}));
+		if (body.message) {
+			response.say(body.message).send();
+		}
+	});
 	return false;
 });
 
 // Intent: LDVPReceiverSetVolumeToIntent
 
 app.intent(INTENT_RECEIVER_SET_VOLUME_TO, _getIntentOpts(INTENT_RECEIVER_SET_VOLUME_TO), function (request, response) {
-	_sendRequest('GET', '/receiver/setVolumeTo/' + request.slot('VOLUME_LEVEL'), {}, _expect([200], function (err, _, body) {
+	const volumeLevel = request.slot('VOLUME_LEVEL');
+	_sendRequest('GET', '/receiver/setVolumeTo/' + volumeLevel, {}, function (err, res, body) {
 		if (err) {
-			console.error(err);
-			response.say("fehler").send();
-			return;
+			response.say("Fehler").send();
 		}
-		console.log(body);
-		response.say("fertig").send();
-	}));
+		if (body.message) {
+			response.say(body.message).send();
+		}
+	});
 	return false;
 });
 
@@ -153,17 +150,6 @@ function _sendRequest(method, path, payload, callback) {
 	console.log('Sending request: ', JSON.stringify(requestOpts));
 
 	request(requestOpts, callback);
-}
-
-function _expect(expectedStatusCodes, callback) {
-	return function (err, res, body) {
-		if (err) {
-			return callback(err);
-		} else if (expectedStatusCodes.indexOf(res.statusCode) === -1) {
-			return callback(new Error('Unexpected status code: ' + res.statusCode + ', expected: ' + expectedStatusCodes));
-		}
-		return callback(null, res, body);
-	};
 }
 
 module.exports = app;
